eb82645db03f11be05d3b92472f398463f4718d4
Michael Hochriegl <michael@hochriegl.dev>
2024-07-25 16:33:16 +0200

    Add ServiceDescriptors for centralized service name handling
    Introduce the `ServiceDescriptors` class with constants for catalog management services. Update project properties by removing certain configurations like `TargetFramework` and `Nullable` and adding `IsAspireProjectResource.

---
+++ src/ServiceDefaults/ServiceDescriptors.cs
@@ -0,0 +1,8 @@
+namespace Microsoft.Extensions.Hosting;
+
+public static class ServiceDescriptors
+{
+    public const string CatalogManagementDbServer = "catalog-management-db-server";
+    public const string CatalogManagementDb = "catalog-management-db";
+    public const string CatalogManagementApi = "catalog-management-api";
+}
