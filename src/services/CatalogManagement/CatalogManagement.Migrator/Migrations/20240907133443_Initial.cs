fb9bfd950912331c3adb144f17d358dafcd4aff1
Michael Hochriegl <michael@hochriegl.dev>
2024-09-07 16:26:23 +0200

    Add initial migration for `CatalogManagement`
    This commit introduces the initial migration for the `CatalogManagement` service. It includes the creation of tables for `CatalogBrands`, `CatalogTypes`, and `CatalogItems` along with their relationships and indexes. It also updates the model snapshot to reflect these changes.

---
+++ src/services/CatalogManagement/CatalogManagement.Migrator/Migrations/20240907133443_Initial.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogManagement.Migrator.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CatalogBrands",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    BrandName = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatalogBrands", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CatalogTypes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatalogTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CatalogItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    AvailableStock = table.Column<long>(type: "bigint", nullable: false),
+                    CatalogBrandId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CatalogTypeId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatalogItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CatalogItems_CatalogBrands_CatalogBrandId",
+                        column: x => x.CatalogBrandId,
+                        principalTable: "CatalogBrands",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CatalogItems_CatalogTypes_CatalogTypeId",
+                        column: x => x.CatalogTypeId,
+                        principalTable: "CatalogTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CatalogItems_CatalogBrandId",
+                table: "CatalogItems",
+                column: "CatalogBrandId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CatalogItems_CatalogTypeId",
+                table: "CatalogItems",
+                column: "CatalogTypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CatalogItems");
+
+            migrationBuilder.DropTable(
+                name: "CatalogBrands");
+
+            migrationBuilder.DropTable(
+                name: "CatalogTypes");
+        }
+    }
+}
